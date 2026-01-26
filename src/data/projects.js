d92a27159eeb59c4c5dbecc2e6c2cc209ff024bf
Maheshvadhiya <maheshvadhiya47@gmail.com>
2026-01-26 19:51:12 +0530

    changes path of project images

--- src/data/projects.js
+++ src/data/projects.js
@@ -5,7 +5,7 @@ const data = [
     description:
       'A community-driven mobile application built for AGRT Samaj, enabling members to connect, engage, and participate in events. Key Contributions: Developed modules for Home, Posts, Events, Members, Business Listings, and Matrimonial. Implemented secure login & profile management. Built event booking for members and guests. Integrated Redux for state management and REST APIs for backend communication. Delivered responsive UI, scalable architecture, and smooth cross-platform performance.',
     image: {
-      src: '0public/project-images/agrt.png',
+      src: 'public/project-images/agrt.png',
       placeholderSrc:
         'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAzIDIiPjwvc3ZnPg==',
     },
@@ -29,7 +29,7 @@ const data = [
     description:
       'A modern social community app with upgraded UI/UX and expanded functionality for projects, jobs, and schemes. Key Contributions: Created pages for Projects, Jobs, Schemes, Members, Posts, Events, Matrimonial, and Business listings. Added event booking and secure authentication system. Implemented modular architecture with Redux and optimized API integrations. Enhanced user experience with clean, modern design and smooth navigation.',
     image: {
-      src: '0public/project-images/ssp.png',
+      src: 'public/project-images/ssp.png',
       placeholderSrc:
         'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAzIDIiPjwvc3ZnPg==',
     },
@@ -54,7 +54,7 @@ const data = [
     description:
       'A complete matrimonial platform for the Kadva Patel community. Key Contributions: Built 10-step registration with save & continue (incomplete profile support). Developed dashboard and profile management. Integrated Firebase push notifications. Added in-app payment system for premium features.',
     image: {
-      src: '0public/project-images/skp.png',
+      src: 'public/project-images/skp.png',
       placeholderSrc:
         'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAzIDIiPjwvc3ZnPg==',
     },
@@ -76,7 +76,7 @@ const data = [
     description:
       'A complete inventory solution with secure login, stock alerts, and sales/purchase analytics. Built Item Master with pagination, filtering, sorting, and bulk actions. Implemented Category, Warehouse, Project, and Role Management with full CRUD. Added multilingual support (EN/HI/GU), detailed reports, profile settings, and customizable fields for scalable inventory operations.',
     image: {
-      src: '0public/project-images/invsys.jpg',
+      src: 'public/project-images/invsys.jpg',
       placeholderSrc:
         'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAzIDIiPjwvc3ZnPg==',
     },
